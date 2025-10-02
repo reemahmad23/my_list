5f875ac25379b309892c66f2b873eeb7c9fd818c
reem ahmad <reemmahmad40@gmail.com>
2025-10-02 15:03:13 +0200

    remove debug check

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       home: MyList(),
     );
   }
